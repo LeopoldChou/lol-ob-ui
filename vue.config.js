ae5b23792a70dda5df9589976fc2e57859907329
leopold <leopoldchou@zhoujun.email.cn>
2025-03-19 21:53:28 +0800

    fix: 请求代理改为仅代理 /api 开头的地址

--- vue.config.js
+++ vue.config.js
@@ -4,11 +4,14 @@ module.exports = defineConfig({
   devServer: {
     port: process.env.VUE_APP_PORT || 8080,
     proxy: {
-      "/": {
+      "/api": {
         ws: false,  // 不代理 WebSocket，不然 HMR 会连不上
         target: "https://127.0.0.1:2999",
         changeOrigin: true,  // 允许跨域
         secure: false,  // HTTPS 而且没有签名证书
+        pathRewrite: {
+          "^/api": "/",
+        }
       }
     }
   }
