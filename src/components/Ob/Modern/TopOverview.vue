a266220309b2cf9300afaa9b81a96294e5af1a17
leopold <leopoldchou@zhoujun.email.cn>
2025-03-20 23:49:11 +0800

    feature: 顶部队伍数据总览组件，events 数据处理

---
+++ src/components/Ob/Modern/TopOverview.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="comp-content">
+    <div class="overview-team">
+      <div class="team-data">
+        <div class="data-icon">男</div>
+        <div class="data-value">{{ topOverviewData.teamOrder.baron }}</div>
+      </div>
+      <div class="team-data">
+        <div class="data-icon">龙</div>
+        <div class="data-value">{{ topOverviewData.teamOrder.dragon }}</div>
+      </div>
+      <div class="team-data">
+        <div class="data-icon">锋</div>
+        <div class="data-value">{{ topOverviewData.teamOrder.xianfeng }}</div>
+      </div>
+      <div class="team-data">
+        <div class="data-icon">虫</div>
+        <div class="data-value">{{ topOverviewData.teamOrder.chaochong }}</div>
+      </div>
+      <div class="team-data">
+        <div class="data-icon">晶</div>
+        <div class="data-value">{{ topOverviewData.teamOrder.inhib }}</div>
+      </div>
+      <div class="team-data">
+        <div class="data-icon">塔</div>
+        <div class="data-value">{{ topOverviewData.teamOrder.turret }}</div>
+      </div>
+      <div class="team-data">
+        <div class="data-icon">金</div>
+        <div class="data-value">{{ topOverviewData.teamOrder.gold }}</div>
+      </div>
+    </div>
+    <div class="overview-middle">
+      <div class="middle-score">
+        {{ topOverviewData.teamOrder.score }}
+        vs
+        {{ topOverviewData.teamChaos.score }}
+      </div>
+      <div class="middle-time">{{ topOverviewData.gameTime }}</div>
+    </div>
+    <div class="overview-team">
+      <div class="team-data">
+        <div class="data-icon">男</div>
+        <div class="data-value">{{ topOverviewData.teamChaos.baron }}</div>
+      </div>
+      <div class="team-data">
+        <div class="data-icon">龙</div>
+        <div class="data-value">{{ topOverviewData.teamChaos.dragon }}</div>
+      </div>
+      <div class="team-data">
+        <div class="data-icon">锋</div>
+        <div class="data-value">{{ topOverviewData.teamChaos.xianfeng }}</div>
+      </div>
+      <div class="team-data">
+        <div class="data-icon">虫</div>
+        <div class="data-value">{{ topOverviewData.teamChaos.chaochong }}</div>
+      </div>
+      <div class="team-data">
+        <div class="data-icon">晶</div>
+        <div class="data-value">{{ topOverviewData.teamChaos.inhib }}</div>
+      </div>
+      <div class="team-data">
+        <div class="data-icon">塔</div>
+        <div class="data-value">{{ topOverviewData.teamChaos.turret }}</div>
+      </div>
+      <div class="team-data">
+        <div class="data-icon">金</div>
+        <div class="data-value">{{ topOverviewData.teamChaos.gold }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TopOverview",
+  props: {
+    topOverviewData: {
+      type: Object,
+      default: () => {
+        return {
+          gameTime: "00:00",
+          teamOrder: { score: 0, gold: 0, turret: 0, inhib: 0, baron: 0, dragon: 0, xianfeng: 0, chaochong: 0, },
+          teamChaos: { score: 0, gold: 0, turret: 0, inhib: 0, baron: 0, dragon: 0, xianfeng: 0, chaochong: 0, },
+        }
+      }
+    }
+  },
+  data() {
+    return {}
+  },
+  methods: {},
+  mounted() {},
+}
+</script>
+
+<style scoped lang="scss">
+.comp-content {
+  width: 100%;
+  height: 100%;
+  background-color: #012;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+}
+.overview-team {
+  display: flex;
+  column-gap: 36px;
+}
+.overview-team:last-child {
+  flex-direction: row-reverse;
+}
+.team-data {
+  display: flex;
+  column-gap: 8px;
+  font-size: 24px;
+}
+.data-icon {}
+.data-value {}
+
+.overview-middle {
+  width: 160px;
+}
+.middle-score {
+  text-align: center;
+  font-size: 32px;
+  line-height: 1em;
+  margin-bottom: 12px;
+}
+.middle-time {
+  text-align: center;
+  font-size: 20px;
+  line-height: 1em;
+}
+</style>
--- src/components/Ob/normal.vue
+++ src/components/Ob/normal.vue
@@ -1,19 +1,29 @@
 <template>
   <div class="comp-content">
-    <div class="top-overview"></div>
+    <div class="top-overview">
+      <top-overview :topOverviewData="topOverviewData"></top-overview>
+    </div>
     <div class="bottom-scoreboard"></div>
   </div>
 </template>
 
 <script>
 import { allGameDataResponse } from "@/components/Ob/temp_allgamedata.js";
+import TopOverview from "@/components/Ob/Modern/TopOverview.vue";
 
 export default {
   name: "obNormal",
+  components: { TopOverview, },
   data() {
     return {
       requestInterval: null,
       allGameData: {},
+      teamMembers: {},
+      topOverviewData: {
+        gameTime: "00:00",
+        teamOrder: { score: 0, gold: 0, turret: 0, inhib: 0, baron: 0, dragon: 0, xianfeng: 0, chaochong: 0, },
+        teamChaos: { score: 0, gold: 0, turret: 0, inhib: 0, baron: 0, dragon: 0, xianfeng: 0, chaochong: 0, },
+      },
     }
   },
   methods: {
@@ -27,11 +37,65 @@ export default {
             console.error(err);
           });
     },
+    convert_gameTime(gameTime) {
+      if (!gameTime || typeof(gameTime) !== "number") {
+        console.log("gameTime", gameTime);
+        return null;
+      }
+      const mm = Math.floor(gameTime / 60).toString().padStart(2, "0");
+      const ss = (Math.floor(gameTime) % 60).toString().padStart(2, "0");
+      return mm + ":" + ss;
+    },
+    get_team_members(allPlayers) {
+      if (!allPlayers) return null;
+      let teamMembers = { teamOrder: [], teamChaos: [], };
+      allPlayers.forEach((item, index) => {
+        if (item.team === "ORDER") teamMembers.teamOrder.push(item.riotIdGameName);
+        else if (item.team === "CHAOS") teamMembers.teamChaos.push(item.riotIdGameName);
+      });
+      return teamMembers;
+    },
+    get_all_event_name(eventsList) {
+      if (!eventsList) return null;
+      let eventsNames = [];
+      eventsList.forEach((item, index) => {
+        if (!eventsNames.includes(item.EventName)) eventsNames.push(item.EventName);
+      });
+      return eventsNames;
+    },
+    get_kill_event_count(eventsList, eventName, teamMembers) {
+      if (!eventsList || !eventName || !teamMembers) return null;
+      let eventsCount = { teamOrder: 0, teamChaos: 0, };
+      eventsList.forEach((item, index) => {
+        if (item.EventName === eventName) {
+          if (teamMembers.teamOrder.includes(item.KillerName)) ++eventsCount.teamOrder;
+          else if (teamMembers.teamChaos.includes(item.KillerName)) ++eventsCount.teamChaos;
+        }
+      });
+      return eventsCount;
+    },
+    update_topOverviewData(eventsList, teamMembers) {
+      const eventCountChampionKill = this.get_kill_event_count(eventsList, "ChampionKill", teamMembers);
+      const eventCountTurretKilled = this.get_kill_event_count(eventsList, "TurretKilled", teamMembers);
+      const eventCountInhibKilled = this.get_kill_event_count(eventsList, "InhibKilled", teamMembers);
+      const eventCountDragonKill = this.get_kill_event_count(eventsList, "DragonKill", teamMembers);
+      this.topOverviewData.teamOrder.score += eventCountChampionKill.teamOrder;
+      this.topOverviewData.teamChaos.score += eventCountChampionKill.teamChaos;
+      this.topOverviewData.teamOrder.turret += eventCountTurretKilled.teamOrder;
+      this.topOverviewData.teamChaos.turret += eventCountTurretKilled.teamChaos;
+      this.topOverviewData.teamChaos.inhib += eventCountInhibKilled.teamChaos;
+      this.topOverviewData.teamChaos.inhib += eventCountInhibKilled.teamChaos;
+      this.topOverviewData.teamChaos.dragon += eventCountDragonKill.teamChaos;
+      this.topOverviewData.teamChaos.dragon += eventCountDragonKill.teamChaos;
+    },
   },
   mounted() {
     // this.request_game_data();
     // this.requestInterval = setInterval(this.request_game_data, 1000);
     this.allGameData = allGameDataResponse.data;
+    this.topOverviewData.gameTime = this.convert_gameTime(this.allGameData.gameData.gameTime);
+    this.teamMembers = this.get_team_members(this.allGameData.allPlayers);
+    this.update_topOverviewData(this.allGameData.events.Events, this.teamMembers);
   },
   beforeDestroy() {
     clearInterval(this.requestInterval);
@@ -40,8 +104,8 @@ export default {
 </script>
 
 <style scoped lang="scss">
-$line-height: 50px;
-$gap: 10px;
+$line-height: 48px;
+$gap: 8px;
 
 .comp-content {
   font-family: Beaufort;
@@ -51,9 +115,8 @@ $gap: 10px;
   top: 0;
   left: 50%;
   transform: translateX(-50%);
-  width: 1200px;
-  height: 54px;
-  background-color: #012;
+  width: 1400px;
+  height: 80px;
 }
 .bottom-scoreboard {
   position: fixed;
